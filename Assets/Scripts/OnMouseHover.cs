2d85d81b48918dd1062e141752ec7575cc617d2d
AHumanBeanCode <powerjmatthew@gmail.com>
2026-09-05 20:12:40 +1200

    enabled preview on mouse-over in ui

--- Assets/Scripts/OnMouseHover.cs
+++ Assets/Scripts/OnMouseHover.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using UnityEngine.EventSystems; // Required for UI pointer events
 
-public class OnMouseHover : MonoBehaviour
+public class UIHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Triggered the exact frame the mouse enters the UI element bounding box
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        // Add hover logic here (e.g., change color, open tooltip)
+        transform.GetChild(0).gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Triggered the exact frame the mouse leaves the UI element bounding box
+    public void OnPointerExit(PointerEventData eventData)
     {
-        
+        // Add reset logic here
+        transform.GetChild(0).gameObject.SetActive(false);
     }
 }
